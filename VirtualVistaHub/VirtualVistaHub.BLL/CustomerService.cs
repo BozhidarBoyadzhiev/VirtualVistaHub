4e0e1125fa43a9ddd8d53b6182e5c98bb86a710e
Божидар Петров Бояджиев <BPBoyadzhiev20@codingburgas.bg>
2024-03-22 14:38:39 +0200

    Add Documentation and initialize asp.net

---
+++ VirtualVistaHub/VirtualVistaHub.BLL/CustomerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualVistaHub.BLL
+{
+    public class CustomerService : ICustomerService
+    {
+
+    }
+}
---
+++ VirtualVistaHub/VirtualVistaHub.PL/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VirtualVistaHub.BLL;
+
+namespace VirtualVistaHub.PL.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
